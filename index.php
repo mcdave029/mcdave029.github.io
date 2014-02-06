b1090a007d10a39aea64a4f373f88de728dc774c
mcdave <mdvaldecantos@gmail.com>
2014-02-06 19:26:20 +0800

    1
    1

--- index.php
+++ index.php
@@ -24,12 +24,14 @@
     <![endif]-->
     <style type="text/css">
 
+    .navbar-static-top {
+      margin-bottom: 19px;
+    }
+
       body
       {
       background-image:url('img/stardust.png');
       background-repeat:repeat;
-      min-height: 2000px;
-      padding-top: 70px;
       }
 
     </style>
@@ -39,13 +41,59 @@
   <body>
 
     <!-- Static navbar -->
-     <?php include('myProfilenav.inc');?>
+    <!-- <div class="navbar navbar-default navbar-static-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../navbar/">Default</a></li>
+            <li class="active"><a href="./">Static top</a></li>
+            <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+          </ul>         
+        </div>
+      </div>
+    </div>-->
 
     <div class="well-lg">
 
 
     <div class="container">
 
+      <!-- Main component for a primary marketing message or call to action -->
+<!--       <div class="jumbotron">
+        <h1>Navbar example</h1>
+        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <p>To see the difference between static and fixed top navbars, just scroll.</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+        </p>
+      </div>
+ -->
       <div class="panel panel-primary"> 
         <!-- Default panel contents -->
         <div class="panel-heading">Infographic Resume <span class="glyphicon glyphicon-send"></span></div>
@@ -53,6 +101,8 @@
         <div class="panel-body">
           <object style="width:100%;height:1200px;" type="application/pdf" data="img/cv.pdf"></object >
         </div>
+
+        <!-- Table -->
         
       </div>
 
