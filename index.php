b147e1ec301de6716e0f872cb5ce839c763afd72
mcdave <mdvaldecantos@gmail.com>
2014-02-06 19:23:23 +0800

    feb 6,2013
    Update my website to php

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>Mc Dave S. Valdecantos</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+    <style type="text/css">
+
+      body
+      {
+      background-image:url('img/stardust.png');
+      background-repeat:repeat;
+      min-height: 2000px;
+      padding-top: 70px;
+      }
+
+    </style>
+
+  </head>
+
+  <body>
+
+    <!-- Static navbar -->
+     <?php include('myProfilenav.inc');?>
+
+    <div class="well-lg">
+
+
+    <div class="container">
+
+      <div class="panel panel-primary"> 
+        <!-- Default panel contents -->
+        <div class="panel-heading">Infographic Resume <span class="glyphicon glyphicon-send"></span></div>
+
+        <div class="panel-body">
+          <object style="width:100%;height:1200px;" type="application/pdf" data="img/cv.pdf"></object >
+        </div>
+        
+      </div>
+
+    </div> <!-- /container -->
+
+</div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-1.10.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
